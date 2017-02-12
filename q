c269fb37920f83180b116a42c0237fcc27b02393
Sergey <tnasergey@gmail.com>
2017-02-12 01:22:16 +0000

    ver 0.02

---
+++ q
@@ -0,0 +1,6 @@
+wget https://github.com/Juggerr/webpack-simple-boilerplate/archive/master.zip
+unzip master.zip
+rm webpack-simple-boilerplate-master/README.md
+cp -r webpack-simple-boilerplate-master/ .
+rm master.zip
+rm -rf webpack-simple-boilerplate-master
--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-//var now = new Date();
-var local_time = 0;
 var time_start_chat = new Date();
 
 var intervalID = window.setInterval(func_interval, 100);
 
 function func_interval() {
+    
     var now = new Date();
     
     var v_hour = now.getHours();
@@ -17,10 +16,17 @@ function func_interval() {
     
     var dif_hour = now.getHours() - time_start_chat.getHours();
     var dif_min = now.getMinutes() - time_start_chat.getMinutes();
-        
-    local_time = now - time_start_chat; 
+
 	document.getElementById("local_time").innerHTML = v_hour + ':' + v_min;
 	document.getElementById("time_online").innerHTML = dif_hour + 'h ' + dif_min + 'm';
-	//document.getElementById("time_online").innerHTML = local_time/(60*60*1000)%24 + 'h ' + local_time/(1000*60)%60 + 'm';
-	//alert(local_time);
+	
+	// пользователей онлайн
+	var count_users = document.getElementsByClassName('list_users').length;
+	document.getElementById('online_users').innerHTML = count_users;
+
 }
+
+function Edtit_Pers_Info() {
+    alert('Edit personal info ');
+}
+
